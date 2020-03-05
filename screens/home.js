e83112c87c2971a9c046ec48f1feecd2f977cddd
bchitester <bchitester@cargurus.com>
2020-03-05 13:08:14 -0500

    background image and nav button

--- screens/home.js
+++ screens/home.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import {
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text,
+    ImageBackground,
+    TouchableOpacity,
+} from 'react-native';
+import backgroundImage from '../assets/background.png';
 
-const Home = () => {
-  return (
-    <>
-      <SafeAreaView>
-        <View style={styles.sectionContainer}>
-          <Text>欢迎</Text>
-          <Text>Read the docs to discover what to do next</Text>
-        </View>
-      </SafeAreaView>
-    </>
-  );
+const Home = ({navigation}) => {
+    return (
+        <ImageBackground
+            source={backgroundImage}
+            style={styles.backgroundImage}>
+            <SafeAreaView>
+                <TouchableOpacity
+                    style={styles.menuIcon}
+                    onPress={() => navigation.openDrawer()}>
+                    <Text>三</Text>
+                </TouchableOpacity>
+                <View>
+                    <Text>欢迎</Text>
+                    <Text>Read the docs to discover what to do next</Text>
+                </View>
+            </SafeAreaView>
+        </ImageBackground>
+    );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    backgroundColor: 'white',
-  },
+    menuIcon: {
+        margin: 10,
+        fontSize: 32,
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover', // or 'stretch'
+    },
 });
 
 export default Home;
